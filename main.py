451ae221bdcfce7f9e9a7436c46dac1a4c824e49
HarryAndrus <56275662+HarryAndrus@users.noreply.github.com>
2024-05-09 16:16:57 -0700

    added support for alternation

---
+++ main.py
@@ -0,0 +1,41 @@
+import pyperclip
+import os
+
+
+def color(word: str) -> str:
+    color1 = "&f&l"
+    alternate_color = ["&a&l","&2&l"]
+    new_word = ""
+    j: bool = False
+
+    for i, char in enumerate(word):
+        if i % 2 == 0:
+            new_word += alternate_color[j] + char
+            j = not j
+        else:
+            new_word += color1 + char
+
+    return new_word
+
+
+def main() -> int:
+    word = str((input("What is the Phrase? ")))
+
+    os.system("cls")
+
+    new_word = color(word)
+    word_length = len(word)
+    new_format_length = len(new_word)
+
+    pyperclip.copy(new_word)
+
+    print(f"Phrase is '{word}'")
+    print(f"Character length: {word_length}")
+    print(f"Length with formatting: {new_format_length}\n")
+    print(f"Formatted Phrase '{new_word}', has been copied to clipboard ")
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
